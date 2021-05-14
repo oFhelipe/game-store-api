177566e145bcef1c0b32b6ca260246616701c0f7
wdwf <w_dwf@hotmail.com>
2021-05-14 20:50:36 -0300

    correção do nome da pasta rotas

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT | 3333;
 
-const routes = require('./Routes')
+const routes = require('./routes')
 
 app.use(cors());
 app.use(express.json());
