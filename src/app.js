76fd511e0f7a36b73c07fc62ac3a374ec0b91bd2
oFhelipe <fhelipe.rodrigues@fatec.sp.gov.br>
2021-06-16 19:41:18 -0300

    teste caminho

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT | 3333;
 
-const routes = require('./routes/index')
+const routes = require('./routes/index.js')
 
 app.use(cors());
 app.use(express.json());
