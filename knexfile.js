8492af99e88f4b4af886767c451e717eb356c86a
oFhelipe <fhelipe.rodrigues@fatec.sp.gov.br>
2021-06-16 19:55:20 -0300

    add host to knexfile

--- knexfile.js
+++ knexfile.js
@@ -16,6 +16,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
+      host: process.env.HOST,
       database: process.env.DATABASE,
       user:     process.env.USER,
       password: process.env.PASSWORD
